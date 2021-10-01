f38a3be3f5f26a4dad48d95c4e0650609c950b7f
Sweettman <91554870+Sweettman@users.noreply.github.com>
2021-10-01 11:42:15 -0600

    Update CSE_01.py
    I need to get this to my branch

--- CSE_01.py
+++ CSE_01.py
@@ -1,2 +1,2 @@
 #Jose LeBaron
-#I'm a chadd at league of legends, and my wife will one day take care of me financially. 
+#I'm a chadd at league of legends, and my wife will one day take care of me financialy
